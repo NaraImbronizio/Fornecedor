dda31a501e9d65d118d847eaba6b1ca4cdea85ef
rslui <rslui@BOOK-CTEUA7J4T8>
2024-10-01 20:24:30 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/FornecedorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class FornecedorController : Controller
+{
+    public IActionResult Index()
+    {
+       
+        var fornecedor = new Fornecedor
+        {
+            Nome = "Magit Tecnologia e Sistemas Ltda",
+            Endereco = "Avenida Presidente Vargas, 1280",
+            Telefone = "(16) 2138-5678"
+        };
+
+        return View(fornecedor);
+    }
+}
---
+++ Models/Fornecedor.cs
@@ -0,0 +1,9 @@
+﻿namespace Fornecedor.Models;
+
+public class Fornecedor
+{
+    public int Id { get; set; }
+    public required string Nome { get; set; }
+    public required string Endereco { get; set; }
+    public required string Telefone { get; set; }
+}
